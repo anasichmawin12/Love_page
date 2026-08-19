e7177a3e3f164e4e2a6e5defd1546201e2235bef
ANAS ICHMAWIN <ichmawinanas93@gmail.com>
2026-08-19 16:25:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let btn=document.querySelector("button")
+let i=document.querySelector("#lasti")
+let div1=document.querySelector(".div3")
+let h3=document.querySelector("h3")
+let a=40
+let b=0
+btn.onclick = () => {
+    a += 40;
+    b+=1
+    i.style.fontSize = `${a}px`;
+    if(b==16){
+        i.style.display = "none";
+        h3.style.display="inline"
+
+    }
+}
